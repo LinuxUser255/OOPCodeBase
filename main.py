ddde1c5f21789d9ab34d07849b00106fae969c03
LinuxUser255 <atnl@protonmail.com>
2026-04-19 19:56:30 -0400

    reformatted refactored main.py to meet best practices

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 #!/usr/bin/env python3
+"""
+OOP demo entry point.
 
-# look in the `src/say_hello/__init__.py` package
-# __init__.py acts as the package's entry point'
-# src is the dir, say_hello.py is the file, Printer is the class
+Demonstrates instantiation and usage of the Printer class.
+
+Package layout:
+    src/say_hello/__init__.py  -- package entry point
+    Printer                    -- the class imported below
+
+Usage:
+    python main.py
+
+Example:
+    hp = Printer(100, 100)
+    hp.check_status()
+    hp.print_document(33)
+    hp.print_document(20)
+"""
+
 from src.say_hello import Printer
 
-"""
-    # How to Create an instance of the printer object
-    hp = Printer()
 
-    # How to test the printer methods
-    print("Initial printer status:")
-    hp.check_status()
+INITIAL_PAGE_CAPACITY = 100
+INITIAL_INK_LEVEL = 100
 
-    print("\nPrinting 33 pages:")
-    hp.print_document(33)
 
-    print("\nPrinting 20 more pages:")
-    hp.print_document(20)
-
-"""
-
-def print_hello():
+def print_hello() -> None:
+    """Print the OOP greeting message."""
     print("Hello, Object-Oriented Programming!")
 
 
-
-def main():
-    # This is where the Printer Object is Created. AKA instance of the Printer Class
-    printer = Printer(100, 100)
+def main() -> None:
+    """Run the Printer demo."""
+    printer = Printer(INITIAL_PAGE_CAPACITY, INITIAL_INK_LEVEL)
     print_hello()
 
     print("\nInitial printer status:")
